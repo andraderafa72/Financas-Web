6e6ed56ba3cf30cf08c3fe923593574c16c1913a
Rafael Andrade <devrafael72@gmail.com>
2021-02-03 13:13:24 -0300

    Alteração de links na sideBar e pagina de transações adicionada

--- routes.js
+++ routes.js
@@ -13,6 +13,7 @@ const loginController = require('./src/controllers/loginController')
 const despesaController = require('./src/controllers/despesaController')
 const receitaController = require('./src/controllers/receitaController')
 const overviewController = require('./src/controllers/overviewController')
+const transacoesController = require('./src/controllers/transacoesController')
 
 const globalMiddleware = require('./src/middlewares/globalMiddleware')
 
@@ -29,16 +30,18 @@ route.get('/register/index', registerController.index);
 route.post('/register/index', registerController.send);
 
 // DESPESAS 
-route.get('/despesas/index', globalMiddleware.loginRequired, despesaController.index);
+route.get('/despesas/index/:id', globalMiddleware.loginRequired, despesaController.index);
 route.get('/despesas/registrar/index', globalMiddleware.loginRequired, despesaController.indexRegister);
 route.post('/despesas/registrar/index', globalMiddleware.loginRequired, despesaController.send);
 
 // RECEITAS 
-route.get('/receitas/index', globalMiddleware.loginRequired, receitaController.index);
+route.get('/receitas/index/:id', globalMiddleware.loginRequired, receitaController.index);
 route.get('/receitas/registrar/index', globalMiddleware.loginRequired, receitaController.indexRegister);
 route.post('/receitas/registrar/index', globalMiddleware.loginRequired, receitaController.send);
 
 route.get('/overview/index/:id', globalMiddleware.loginRequired, overviewController.index);
+
+route.get('/transacoes/index/:id', globalMiddleware.loginRequired, transacoesController.index);
 
 route.get('/api/overview/chart',  globalMiddleware.loginRequired, globalMiddleware.chartData)
 
