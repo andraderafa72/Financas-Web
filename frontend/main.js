45f11db6213308dc4ef3c927ec9111f17f9133d9
Rafael Andrade <devrafael72@gmail.com>
2021-02-18 18:35:27 -0300

    Loading screen added

--- frontend/main.js
+++ frontend/main.js
@@ -8,6 +8,17 @@ import './assets/modules/overviewChart';
 import './assets/modules/transacoes';
 
 // JQUERY
+
+$(window).on('load', function(){
+  setTimeout(removeLoader, 2000);
+});
+function removeLoader(){
+    $( ".d-loading" ).fadeOut(500, function() {
+      $( ".d-loading" ).remove();
+      $('.app-container').toggleClass('block')
+  });  
+}
+
 $(".burger-squeeze").click(function(){  
   $(".burger-squeeze").toggleClass("open");  
   $(".side-bar").toggleClass("toggler");
